d83c5261b54dc851b6fce7dd7cd8bbdcb18c17e3
hahahhhh <pkq_q@163.com>
2020-08-24 11:35:48 +0800

    some modifies

--- src/main/java/test.java
+++
@@ -1,9 +0,0 @@
-import entity.keyboard.KeyBoard_Stage2;
-
-public class test {
-    public static void main(String[] args) {
-        KeyBoard_Stage2 keyBoard_stage2 = new KeyBoard_Stage2();
-        System.out.println(keyBoard_stage2.dial(3,7,8).output());
-    }
-
-}
